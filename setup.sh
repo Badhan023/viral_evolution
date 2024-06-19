a0e683d624072add40ca8e84fe04d667a9d8a5cb
Badhan Das <badhan.2192@gmail.com>
2024-06-19 18:15:30 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+##installing pyani
+conda install --yes pyani
+
+##installing sourmash
+conda install -c conda-forge sourmash-minimal
+
+##installing blast
+conda install blast
+
+#install modules
+conda install anaconda::pandas
+conda install -c conda-forge biopython
+conda install editdistance
+conda install numpy
+pip install networkx
+pip install matplotlib
